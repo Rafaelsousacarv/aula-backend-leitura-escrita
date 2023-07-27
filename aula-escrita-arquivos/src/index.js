d644ed05fcbebcab5911675ed252d7be87257de0
Rafael Sousa <rafiinhaa.sousa@hotmail.com>
2023-07-27 11:56:10 -0300

    Aula escrita em arquivos

---
+++ aula-escrita-arquivos/src/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const fs = require("fs/promises");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const texto = "Tudo bem";
+  await fs.writeFile("./src/b.txt", texto);
+
+  res.send("Pagina Inicial");
+});
+
+app.post("/", async (req, res) => {
+  const { nome, idade } = req.body;
+  try {
+    const teste = await fs.readFile("./src/usuarios.json");
+
+    const pessoas = JSON.parse(teste);
+
+    pessoas.push({ nome, idade });
+
+    const pessoasStringify = JSON.stringify(pessoas);
+
+    await fs.writeFile("./src/usuarios.json", pessoasStringify);
+
+    return res.json("Pessoa cadastrada com sucesso.");
+  } catch (erro) {
+    return res.json(`Deu erro: ${erro.message}`);
+  } finally {
+    console.log("isso sempre será executado");
+  }
+});
+
+app.listen(3000);
